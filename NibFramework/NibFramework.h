f94d90a357c21b989397c88d4442df9cc403f0db
Rishab Dutta <rishab.dutta@inmobi.com>
2021-11-23 22:33:11 +0530

    Initial Commit

---
+++ NibFramework/NibFramework.h
@@ -0,0 +1,18 @@
+//
+//  NibFramework.h
+//  NibFramework
+//
+//  Created by Rishab Dutta on 23/11/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NibFramework.
+FOUNDATION_EXPORT double NibFrameworkVersionNumber;
+
+//! Project version string for NibFramework.
+FOUNDATION_EXPORT const unsigned char NibFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NibFramework/PublicHeader.h>
+
+
